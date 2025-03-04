c00f5c6885719d9539746a511542bf7382a851bc
banskotanup <baskota.anup11@gmail.com>
2025-03-04 21:41:00 +0545

    Add db

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 module.exports = new Pool({
-    host: "localhost",
-    user: "sysadmin",
-    database: "minimessageboard",
-    password: "Stdy4drm",
-    port: "5432",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
