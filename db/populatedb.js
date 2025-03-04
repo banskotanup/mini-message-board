28fa837c20faccbb2a459b0aab48c205090f80ef
banskotanup <baskota.anup11@gmail.com>
2025-03-04 21:54:20 +0545

    pop db

--- db/populatedb.js
+++ db/populatedb.js
@@ -1,6 +1,14 @@
 const { Client } = require("pg");
 const SQL = `
-    
+    CREATE TABLE messages (
+        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        username VARCHAR (255),
+        messages TEXT,
+        created_at DATE DEFAULT NOW()
+    );
+
+    INSERT INTO messages (username, messages)
+    VALUES ('Anup', 'Hi I am Anup')
 `;
 
 async function main() {
